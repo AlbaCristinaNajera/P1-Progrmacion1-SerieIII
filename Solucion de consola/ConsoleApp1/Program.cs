2d59c7b3fcd05b75b824be84c23323d55d886680
Cristina_nm <anajeram@miumg.edu.gt>
2024-03-09 13:10:27 -0600

    first commit

---
+++ Solucion de consola/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la calculadora de precios");
+
+            Console.WriteLine("Ingrese el precio del primer producto");
+            int precio1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el precio del segundo producto");
+            int precio2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el precio del tercer producto");
+            int precio3 = Convert.ToInt32(Console.ReadLine());
+
+            int total = precio1 + precio2 + precio3;
+
+            if (total > 500) 
+            {
+                total = total - (total * 10 / 100);
+            }
+            Console.WriteLine("El total de los productos es: " + total + " centimos");
+            Console.WriteLine("Gracias por usar el programa");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ gmail/gmail/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al programa");
+
+            Console.WriteLine("Por favor ingrese su nombre");
+            string nombre= Console.ReadLine();
+            Console.WriteLine("Por favor ingrese su apellido");
+            string apellido = Console.ReadLine();
+            
+            string correo = generarCorreo(nombre,apellido);
+
+            Console.WriteLine("Tu cuenta de correo electronico es: " + correo);
+            Console.ReadLine();
+        }
+        static string generarCorreo(string nombre, string apellido)
+        {
+            string nombreCorto = nombre.Substring(0, Math.min(nombre.Length, 3);
+            string apellidoCorto = nombre.Substring(0, Math.min(apellido.Length, 3);
+        }
+    }
+}
